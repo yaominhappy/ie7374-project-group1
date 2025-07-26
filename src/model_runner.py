05365a00c7a36c60e5c955cb808c3e0f5abc5866
Yao Min <yaomin@192-168-1-80.tpgi.com.au>
2025-07-26 23:37:23 +1000

    update BERT model

--- src/model_runner.py
+++ src/model_runner.py
@@ -455,59 +455,65 @@ class LSTMEmotionModel(tf.keras.Model):
         }
 
 """### BERT-based Multi-Task Model"""
+# Disable mixed precision for inference to avoid dtype issues
+tf.keras.mixed_precision.set_global_policy('float32')
 
 class BERTMultiTaskModel(tf.keras.Model):
     """BERT model for multi-task emotion and sentiment classification"""
     def __init__(self, model_name='bert-base-uncased', num_emotions=7,
                  num_sentiments=3, dropout_rate=0.3):
         super(BERTMultiTaskModel, self).__init__()
-
+        
         # BERT encoder
         self.bert = TFAutoModel.from_pretrained(model_name)
         hidden_size = self.bert.config.hidden_size
-
+        
         # Freeze BERT layers initially (optional)
         self.bert.trainable = True
-
+        
         # Dropout
-        self.dropout = layers.Dropout(dropout_rate)
-
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+        
         # Emotion classification head
-        self.emotion_dense1 = layers.Dense(hidden_size, activation='relu')
-        self.emotion_dropout = layers.Dropout(dropout_rate)
-        self.emotion_dense2 = layers.Dense(hidden_size // 2, activation='relu')
-        self.emotion_output = layers.Dense(num_emotions, name='emotion')
-
+        self.emotion_dense1 = tf.keras.layers.Dense(hidden_size, activation='relu')
+        self.emotion_dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.emotion_dense2 = tf.keras.layers.Dense(hidden_size // 2, activation='relu')
+        self.emotion_output = tf.keras.layers.Dense(num_emotions, name='emotion')
+        
         # Sentiment classification head
-        self.sentiment_dense1 = layers.Dense(hidden_size, activation='relu')
-        self.sentiment_dropout = layers.Dropout(dropout_rate)
-        self.sentiment_dense2 = layers.Dense(hidden_size // 2, activation='relu')
-        self.sentiment_output = layers.Dense(num_sentiments, name='sentiment')
-
+        self.sentiment_dense1 = tf.keras.layers.Dense(hidden_size, activation='relu')
+        self.sentiment_dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.sentiment_dense2 = tf.keras.layers.Dense(hidden_size // 2, activation='relu')
+        self.sentiment_output = tf.keras.layers.Dense(num_sentiments, name='sentiment')
+    
     def call(self, inputs, training=False):
+        # Ensure input_ids are int32
+        input_ids = tf.cast(inputs['input_ids'], tf.int32)
+        attention_mask = tf.cast(inputs['attention_mask'], tf.int32)
+        
         # BERT encoding
         bert_outputs = self.bert(
-            input_ids=inputs['input_ids'],
-            attention_mask=inputs['attention_mask'],
+            input_ids=input_ids,
+            attention_mask=attention_mask,
             training=training
         )
-
+        
         # Use pooled output (CLS token)
         pooled_output = bert_outputs.pooler_output
         pooled_output = self.dropout(pooled_output, training=training)
-
+        
         # Emotion prediction
         emotion_x = self.emotion_dense1(pooled_output)
         emotion_x = self.emotion_dropout(emotion_x, training=training)
         emotion_x = self.emotion_dense2(emotion_x)
         emotion_logits = self.emotion_output(emotion_x)
-
+        
         # Sentiment prediction
         sentiment_x = self.sentiment_dense1(pooled_output)
         sentiment_x = self.sentiment_dropout(sentiment_x, training=training)
         sentiment_x = self.sentiment_dense2(sentiment_x)
         sentiment_logits = self.sentiment_output(sentiment_x)
-
+        
         return {
             'emotion': emotion_logits,
             'sentiment': sentiment_logits
